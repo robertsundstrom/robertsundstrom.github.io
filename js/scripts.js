e3dac596ee09b8f8d072ce6b4f283ad5ba0b2467
Robert Sundström <robert.sundstrom@outlook.com>
2020-02-09 12:31:32 +0100

    Update app

--- js/scripts.js
+++ js/scripts.js
@@ -73,37 +73,3 @@ window.openInNewTab = function (href) {
         href: href,
     }).click();
 };
-// @ts-nocheck
-window.toc = {
-    generate: function (element) {
-        var toc = "";
-        var level = 1;
-        var tocElements = element.getElementsByClassName("toc");
-        if (tocElements.length === 0) {
-            return;
-        }
-        var tocElement = tocElements.item(0);
-        element.innerHTML =
-            element.innerHTML.replace(/<h([\d])>([^<]+)<\/h([\d])>/gi, function (str, openLevel, titleText, closeLevel) {
-                if (openLevel != closeLevel) {
-                    return str;
-                }
-                if (openLevel > level) {
-                    toc += (new Array(openLevel - level + 1)).join("<ul>");
-                }
-                else if (openLevel < level) {
-                    toc += (new Array(level - openLevel + 1)).join("</ul>");
-                }
-                level = parseInt(openLevel);
-                var anchor = titleText.replace(/ /g, "_");
-                toc += "<li><a href=\"#" + anchor + "\">" + titleText
-                    + "</a></li>";
-                return "<h" + openLevel + "><a name=\"" + anchor + "\">"
-                    + titleText + "</a></h" + closeLevel + ">";
-            });
-        if (level) {
-            toc += (new Array(level + 1)).join("</ul>");
-        }
-        tocElement.innerHTML += " " + toc.trim();
-    }
-};
